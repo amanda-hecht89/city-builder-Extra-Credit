7a23bdcd2c04d8a17323192f2cf330185aee4254
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-04-28 14:01:43 -0600

    lunch

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // import functions and grab DOM elements
+const FunDrop = document.getElementById('fundrop');
+const SeaDrop = document.getElementById('seadrop');
+const Fun = document.getElementById('')
+const Sea = document.getElementById('');
 
 // let state
 
@@ -6,3 +10,12 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+  FunDrop.addEventListener('change', () => {
+    console.log('changing select', FunDrop.value);
+    Fun.style.backgroundImage = `url('../assets/${FunDrop.value}.png')`;
+
+
+
+  SeaDrop.addEventListener('change', () => {
+      console.log('changing select', SeaDrop.value);
+      Sea.style.backgroundImage = `url('../assets/${SeaDrop.value}.png')`; 
