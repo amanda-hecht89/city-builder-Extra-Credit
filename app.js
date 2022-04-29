9b6d00af5c4af6ad37d1ac2e92babdaeb82c62b2
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-04-28 18:53:09 -0600

    end of day

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // import functions and grab DOM elements
 const funDrop = document.getElementById('fundrop');
 const seaDrop = document.getElementById('seadrop');
+const foodDrop = document.getElementById('fooddrop');
 const fun = document.getElementById('fun');
 const sea = document.getElementById('season');
+const food = document.getElementById(foodie);
 const sloganEl = document.getElementById('slogan');
 const sloganInput = document.getElementById('slogan-input');
 const sloganButton = document.getElementById('slogan-button');
@@ -16,7 +18,6 @@ let comment = [];
   // update DOM to reflect the new state
 funDrop.addEventListener('change', () => {
     console.log('changing select', funDrop.value);
-    console.log(fun);
     fun.style.backgroundImage = `url('./assets/${funDrop.value}.png')`;
 });
 
@@ -24,6 +25,11 @@ funDrop.addEventListener('change', () => {
 seaDrop.addEventListener('change', () => {
     console.log('changing select', seaDrop.value);
     sea.style.backgroundImage = `url("./assets/${seaDrop.value}.png")`; 
+});
+
+foodDrop.addEventListener('change', () => {
+    console.log('changing select', foodDrop.value);
+    food.style.backgroundImage = `url('./assets/${foodDrop.value}.png')`;
 });
 
 sloganButton.addEventListener('click', () => {
